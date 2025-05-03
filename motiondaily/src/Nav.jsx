925f51b37d25e9cc664e4927dc300df32cd2b338
wareez92 <wareezsowemimo@gmail.com>
2025-05-03 15:19:29 -0500

    navbar component built

---
+++ motiondaily/src/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav () {
+ <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
+ </ul>
+}
+
+export default Nav;
