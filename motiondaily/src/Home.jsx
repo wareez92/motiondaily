a59f0dce9bfc117866f2b07bd779fb392a65bafd
wareez92 <wareezsowemimo@gmail.com>
2025-05-05 15:58:42 -0500

    why us section created

--- motiondaily/src/Home.jsx
+++ motiondaily/src/Home.jsx
@@ -1,6 +1,7 @@
 import Nav from "./Nav";
 import Hero from "./Hero";
 import Promo from "./Promo";
+import WhyUs from "./WhyUs";
 
 function Home({promoPacks}) {
   return (
@@ -8,6 +9,7 @@ function Home({promoPacks}) {
       <Nav />
       <Hero />
       <Promo promoPacks={promoPacks} />
+      <WhyUs/>
     </>
   );
 }
---
+++ motiondaily/src/WhyUs.jsx
@@ -0,0 +1,37 @@
+function WhyUs() {
+  return (
+    <>
+      <section>
+        <div>
+          <img src="null" alt="icon" />
+          <h4>Input Info</h4>
+          <ul>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+          </ul>
+        </div>
+        <div>
+          <img src="null" alt="icon" />
+          <h4>Input Info</h4>
+          <ul>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+          </ul>
+        </div>
+        <div>
+          <img src="null" alt="icon" />
+          <h4>Input Info</h4>
+          <ul>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default WhyUs;
